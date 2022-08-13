224d9de8161bfe6a60748afbfdcc6cd235406990
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-08-13 02:19:55 +0200

    Init commit

---
+++ src/main/java/com/mealdash/controllers/TestController.java
@@ -0,0 +1,14 @@
+package com.mealdash.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+	@RequestMapping("/get")
+	public String get() {
+		return "/test/get";
+	}
+}
